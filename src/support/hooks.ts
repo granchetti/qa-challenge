0dfbb455a5846a30ff15252d15ea50812b97fd32
granchetti1 <134518621+granchetti1@users.noreply.github.com>
2025-02-24 00:18:48 +0100

    Fix workflow

--- src/support/hooks.ts
+++ src/support/hooks.ts
@@ -3,29 +3,28 @@ import { Browser, BrowserContext, Page, chromium } from "@playwright/test";
 import fs from "fs";
 import path from "path";
 
-setDefaultTimeout(30000)
+setDefaultTimeout(30000);
 
 let browser: Browser;
 export let context: BrowserContext;
 export let page: Page;
 
-Before(async function() {
-  const isCI = process.env.CI === 'true';
+Before(async function () {
+  const isCI = process.env.CI === "true";
   browser = await chromium.launch({ headless: isCI });
-  
   const userKey = this.parameters.userKey || "default";
-  
   const authFilePath = path.resolve(`auth/${userKey}.json`);
   context = await browser.newContext(
     fs.existsSync(authFilePath) ? { storageState: authFilePath } : {}
   );
-
   page = await context.newPage();
 });
 
 After(async function (scenario) {
   if (scenario.result?.status === Status.FAILED) {
-    await page.screenshot({ path: `screenshots/${scenario.pickle.name}.png` });
+    const screenshotPath = `reports/screenshots/${scenario.pickle.name}.png`;
+    await page.screenshot({ path: screenshotPath });
+    this.attach(fs.readFileSync(screenshotPath), "image/png");
   }
   await context.close();
   await browser.close();
